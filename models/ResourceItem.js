21246de3506818df7bd21096501b2c508e465e94
Anton Zuev <anzuev@bk.ru>
2017-06-16 11:38:45 +0300

    Changed sorting of unwatched resource items

--- models/ResourceItem.js
+++ models/ResourceItem.js
@@ -130,7 +130,7 @@ ResourceItem.statics.getUnwatchedVideos = function (options) {
         filterObj.watchedBy = mongoose.Types.ObjectId(options.userId);
     }
     return this.find(filterObj)
-        .sort({created: 1})
+        .sort({created: -1})
         .skip((options.skip || 0)*config.appearence.videosPerPage)
         .limit(config.appearence.videosPerPage)
         .select({"url": 1, "tags": 1, "title": 1})
